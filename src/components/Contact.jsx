0e5a996186e56a4f446871c055b7668b067d5a85
Shaswat Shukla <shuklashashwat482@gmail.com>
2022-09-17 03:19:05 -0600

    fixed contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,97 +1,124 @@
-import React, { useState } from 'react';
+// This is the contact form component, which is mostly a form for the user to fill out in order to send me a message 
+
+// To receive email when a user sends me a message through the website, I have used Formspree, which allows me to receive an email without using any backend code 
+// react-hook-form package is used for form validation.
+// react-toastify package is used for UX (user experience) as after the form is submitted, it creates a little alert card at the bottom right of the user screen to say that the form was been successfully sent
+// Axios is used for making HTTP requests from the browser
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import {useForm} from "react-hook-form"
+import React from 'react'
 import axios from 'axios';
 
-export default () => {
-  const [status, setStatus] = useState({
-    submitted: false,
-    submitting: false,
-    info: { error: false, msg: null },
-  });
-  const [inputs, setInputs] = useState({
-    email: '',
-    message: '',
-  });
-  const handleServerResponse = (ok, msg) => {
-    if (ok) {
-      setStatus({
-        submitted: true,
-        submitting: false,
-        info: { error: false, msg: msg },
-      });
-      setInputs({
-        email: '',
-        message: '',
-      });
-    } else {
-      setStatus({
-        info: { error: true, msg: msg },
-      });
-    }
-  };
-  const handleOnChange = (e) => {
-    e.persist();
-    setInputs((prev) => ({
-      ...prev,
-      [e.target.id]: e.target.value,
-    }));
-    setStatus({
-      submitted: false,
-      submitting: false,
-      info: { error: false, msg: null },
+function Contact(){  
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState:{errors}
+  } = useForm()
+
+  // This function styles the notification that the user will see after the form is submitted
+
+  const toastifySuccess = () => {
+    toast('Form was successfully sent to Shaswat Shukla', {
+      position: 'bottom-right',
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,  
+      draggable: false,
+      className: 'submit-feedback success',
+      toastId: 'notifyToast'
     });
   };
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
-    axios({
-      method: 'POST',
-      url: 'https://formspree.io/f/mknejqva',
-      data: inputs,
-    })
-      .then((response) => {
-        handleServerResponse(
-          true,
-          'Thank you, your message has been submitted.',
-        );
-      })
-      .catch((error) => {
-        handleServerResponse(false, error.response.data.error);
-      });
-  };
+
+  // Catching the submit event on a form
+
+  const onSubmit = async (data) =>{
+
+    // storing the data enetered by the user
+
+    const { name, email, subject, message } = data;
+    try {
+      const dataObject = {
+        name,
+        email,
+        subject,
+        message
+      };
+
+      // sending post request
+
+      const response=await axios.post(
+        "https://formspree.io/f/mknejqva",dataObject
+      );
+      reset();
+      toastifySuccess();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+
   return (
-    <main>
-      <h1>React and Formspree</h1>
-      <hr />
-      <form onSubmit={handleOnSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="email"
-          name="_replyto"
-          onChange={handleOnChange}
-          required
-          value={inputs.email}
-        />
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          name="message"
-          onChange={handleOnChange}
-          required
-          value={inputs.message}
-        />
-        <button type="submit" disabled={status.submitting}>
-          {!status.submitting
-            ? !status.submitted
-              ? 'Submit'
-              : 'Submitted'
-            : 'Submitting...'}
-        </button>
+    <div className='contact_section' id='contact'>
+      <h1>Contact Me</h1>
+
+      {/* form starts */}
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='contact_content'>
+
+        {/* Name of the user*/}
+
+          <div className='input_group'>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" placeholder="Your name" className='name'
+              {...register('name', {required: { value: true, message: 'Please enter your name' }, maxLength: {value: 30, message: 'Please use 30 characters or less'}})}
+            />
+            {errors.name && <div className='error_message'>{errors.name.message}</div>}
+          </div>
+
+          {/* Email of the user */}
+
+          <div className='input_group'>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="Your email" className='email'
+              {...register('email', {required: true,pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/})}
+            />
+            {errors.email && (<div className='error_message'>Please enter a valid email address</div>)}
+          </div>
+
+          {/* Subject of the message */}
+
+          <div className='input_group'>
+            <label htmlFor="subject">Subject</label>
+            <input type="text" name="subject" placeholder="Subject"
+              {...register('subject', {required: { value: true, message: 'Please enter a subject' },maxLength: {value: 75, message: 'Subject cannot exceed 75 characters'}})}
+            />
+            {errors.subject && (<div className='error_message'>{errors.subject.message}</div>)}
+          </div>
+
+          {/* Message from the user */}
+
+          <div className='input_group'>
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" cols="30" rows="10" placeholder="Message" 
+              {...register('message', {required: true})}
+            ></textarea>
+            {errors.message && <div className='error_message'>Please enter a message</div>}
+          </div>
+
+          <button type="submit" className='submit_btn'>Send Message</button>
+
+        </div>  
       </form>
-      {status.info.error && (
-        <div className="error">Error: {status.info.msg}</div>
-      )}
-      {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
-    </main>
+        <ToastContainer/>
+    </div>
   );
-};
+}        
+
+export default Contact;
