f3eec0c366562eae6b923055fa5d319c345cc328
testing <shuklashashwat482@gmail.com>
2023-07-17 02:28:24 -0600

    changing mobile styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,9 +11,9 @@ function About() {
       <a href="https://github.com/SHASWAT2000-loves-logic?tab=repositories" target="_blank"><FaGithub className="about_icons"/></a> 
       <p>I am a recent CS graduate from the University of New Mexico (UNM) working as a software developer at the New Mexico Environment Department. Previously, I built a website from scratch for the Great Plains Partners Venture Group startup and also completed a software engineering internship at New Mexico Administrative Office of the District Attorneys. Before that, I did research projects at UNM, Stanford and Purdue, working in different areas of computer science such as robotics, IoT and deep learning.</p>
       <br />
-      <p><span className="bold">Interests</span> – Frontend development, full stack development, UI/UX</p>
-      <p><span className="bold">Proficient</span> – HTML, CSS (SASS/LESS), JavaScript (React, Angular, Node), Python (Flask), SQL</p>
-      <p><span className="bold">Tools</span> – AWS, Git, Netlify, Vercel, VS Code</p>
+      <p className="additional_info"><span className="bold">Interests</span> – Frontend development, full stack development, UI/UX</p>
+      <p className="additional_info"><span className="bold">Proficient</span> – HTML, CSS (SASS/LESS), JavaScript (React, Angular, Node), Python (Flask), SQL</p>
+      <p className="additional_info"><span className="bold">Tools</span> – AWS, Git, Netlify, Vercel, VS Code</p>
       <br />
 
       <a href={Resume} target="_blank" className="resume"><span className="highlighted">Resume</span></a>
